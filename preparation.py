8e9468e4f7016ab99a8e1614fdffe76c6d468a1b
Laroussestela <62725709+Laroussestela@users.noreply.github.com>
2025-04-14 11:59:54 +0200

    Update preparation.py

--- preparation.py
+++ preparation.py
@@ -129,3 +129,12 @@ def data_cleaning():
   df_train['int_col_second'] = df_train['int_col_second'].replace('Ebony Accents', 'Ebony')
   df_train['int_col_second'] = df_train['int_col_second'].replace('Ebony/Ebony', 'Ebony')
   df_train['int_col_second'] = df_train['int_col_second'].replace('Saddle Brown', 'Saddle')
+
+def remove_outliers(df, column):   
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1   
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR    
+    df_out = df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
+    return df_out
